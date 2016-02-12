81a665e8a3c37ef4b95611222e0ada193806b600
Anton Kadantsev <kadantsev.anton@yandex.ru>
2016-02-12 11:39:26 +0300

    8

--- wp-content/plugins/wfm-related-posts/wfm-related-posts.php
+++ wp-content/plugins/wfm-related-posts/wfm-related-posts.php
@@ -5,13 +5,13 @@ Description: Выводит похожие посты
 Version: Номер версии плагина, например: 1.0
 Author: Тоха
 */
-add_filter('the_content', 'wfm_related_posts');//установить фильтр к функцие the_content
+add_filter('the_content', 'wfm_related_posts');//установить фильтр к функции the_content
 
 function wfm_related_posts($content){
 	if(!is_single()) return $content;//если мы находимся вне записи, то вывести контент
-    echo $id = get_the_ID();//получаем id текущей записи. По этому ID будем искать категории
-    $categories = get_the_category($id);//получим массим категорий
-    foreach($categories as $category){//переберем массив с объктом и выберем только ID категорий
+    $id = get_the_ID();//получаем id текущей записи. По этому ID будем искать категории
+    $categories = get_the_category($id);//получим массив категорий
+    foreach($categories as $category){//переберем массив с объектом и выберем только ID категорий
         $cats_id[] = $category->cat_ID;
     }
     $related_posts = new WP_Query(array(//создадим объект только с теми записями в которых есть найденные категории
@@ -24,10 +24,15 @@ function wfm_related_posts($content){
         $content .= '<div class="related-posts"><h3>Возможно вас заинтересуют эти записи:</h3>';
         while($related_posts->have_posts()){//запускаем цикл wordpress
 		$related_posts->the_post();//если это не указать то записи не будут перебираться и всё зациклется
-		$content .= '<a href="' . get_permalink() . '">' . get_the_title() . '</a><br>';//составим ссылку
+		if(has_post_thumbnail()){
+			$img = get_the_post_thumbnail(get_the_ID(), array(100, 100), array('alt'=>'', 'title'=>get_the_title()));
+		}else{
+			$img = '<img src="'. plugins_url('image/no_img.jpg', __FILE__) .'" alt="" title="'.get_the_title().'" width="100" height="100">';
+		}
+		$content .= '<a href="' . get_permalink() . '">' . $img . '</a>';//составим ссылку
         }
         $content .= '</div>';
-        wp_reset_query();//сбросить глобальные атрибуты, если не сделать, то будут работать не корректно остальные плагины
+        wp_reset_query();//сбросить глобальные атрибуты, если не сделать, то будут работать некорректно остальные плагины
     }
 	return $content;
 }
