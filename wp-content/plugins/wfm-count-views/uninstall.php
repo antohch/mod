a051781fcedb018cc8abd4fc46f49bc34f05bb35
Anton Kadantsev <kadantsev.anton@yandex.ru>
2016-02-13 16:25:00 +0300

    12

--- wp-content/plugins/wfm-count-views/uninstall.php
+++ wp-content/plugins/wfm-count-views/uninstall.php
@@ -1,7 +1,9 @@
 <?php
 if(!defined('WP_UNINSTALL_PLUGIN')) exit;
-
-global $wpdb;
-$query = "ALTER TABLE $wpdb->posts DROP wfm_views";
-$wpdb->query($query);
+include dirname(__FILE__).'/wfm-check.php';
+if(wfm_check_field('wfm_views')){
+	global $wpdb;
+	$query = "ALTER TABLE $wpdb->posts DROP wfm_views";
+	$wpdb->query($query);
+}
 
