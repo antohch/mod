eadcf4223bfe9a2940448e87cb4bf71909e7b32e
Anton Kadantsev <kadantsev.anton@yandex.ru>
2016-02-15 13:54:16 +0300

    14-2

--- wp-content/plugins/wfm-capcha-login/wfm-capcha-login.php
+++ wp-content/plugins/wfm-capcha-login/wfm-capcha-login.php
@@ -14,10 +14,18 @@ add_action('login_form', 'wfm_captcha_login');
 add_action('wp_authenticate', 'wfm_wp_authenticate', 10, 2);
 
 function wfm_wp_authenticate($username, $password){
-	var_dump($_POST);
+	if(isset($_POST['check']) && $_POST['check'] == 'check'){
+		//wp_die('<b>Ошибка</b>: Вы не прошли проверку на человечность');
+		add_filter('login_errors', 'wfm_login_errors_my');
+		$username = null;
+	}
 }
 
 function wfm_captcha_login(){
 	echo '<p><label for="check"><input type="checkbox" name="check" id="check" value="check" checked> Снимите галочку</label></p>';
+}
+
+function wfm_login_errors_my(){
+	return '<b>Ошибка</b>: Вы не прошли проверку на человечность';
 }
 
