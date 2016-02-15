9f34ed329a08bac992741332aa861de722bcd6c2
Anton Kadantsev <kadantsev.anton@yandex.ru>
2016-02-15 16:59:35 +0300

    15

--- wp-content/plugins/wfm-capcha-login/wfm-capcha-login.php
+++ wp-content/plugins/wfm-capcha-login/wfm-capcha-login.php
@@ -9,7 +9,7 @@ Author: Тоха
 
 function wfm_login_errors_my(){
 	return "Ошибка авторизации";
-}*/
+}
 add_action('login_form', 'wfm_captcha_login');
 add_action('wp_authenticate', 'wfm_wp_authenticate', 10, 2);
 
@@ -27,5 +27,22 @@ function wfm_captcha_login(){
 
 function wfm_login_errors_my(){
 	return '<b>Ошибка</b>: Вы не прошли проверку на человечность';
+}
+*/
+add_action('login_form', 'wfm_captcha_login');
+add_filter('authenticate', 'wp_auth_signon', 30, 3);//3-количество параметров
+
+function wfm_captcha_login(){
+	echo '<p><label for="check"><input type="checkbox" name="check" id="check" value="check" checked> Снимите галочку</label></p>';
+}
+
+function wp_auth_signon($user, $username, $password){
+	if(isset($_POST['check']) && $_POST['check'] == 'check'){
+		$user = new WP_Error('broke', '<b>Ошибка</b>: вы бот?');
+	}
+	if(isset($user->errors['incorrect_password']) || isset($user->errors['invalid_username'])){
+		$user = new WP_Error('broke', '<b>Ошибка</b>: неверный логин или пароль');
+	}
+	return $user;
 }
 
