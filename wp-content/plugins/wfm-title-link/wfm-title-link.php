30e75c43e0e18dbbea76c2d56c232a03e2741870
Anton Kadantsev <kadantsev.anton@yandex.ru>
2016-02-17 16:03:45 +0300

    17

--- wp-content/plugins/wfm-title-link/wfm-title-link.php
+++ wp-content/plugins/wfm-title-link/wfm-title-link.php
@@ -26,6 +26,36 @@ function wfm_title($title){
 	elseif(is_search()){
 		$title = array('Результаты поиска по запросу:'. get_search_query());
 	}
+	//категории
+	elseif(is_category()){
+		//id категории
+		$cat_id = get_query_var('cat');
+		//данные категории
+		if($cat->parent){
+			//если есть родительская категория
+			$categories = rtrim(get_category_parents($cat_id, false, $sep), $sep);
+			$categories = explode($sep, $categories);
+			$categories[] = $site;
+			$categories = array_reverse($categories);
+			$title[] = $site;
+		}else{
+			//если это самостоятельная категория
+			$title = array($cat->name, $site);
+		}
+	}
+	// записи
+	if(is_single()){
+		//массив данных о категориях
+		$category = get_the_category();
+		//ID категории
+		$cat_id = $category[0]->cat_ID;
+		//родительские категории
+		$categories = rtrim(get_category_parents($cat_id, false, $sep), $sep);
+		$categories = explode($sep, $categories);
+		$categories = get_the_title();
+		$categories = array_reverse($categories);
+		$title[] = $site;
+	}
 	//архив
 	elseif(is_archive()){
 		$title = array('Архив за:' . get_the_time("F Y"), $site);
---
+++ wp-content/plugins/wfm-title/wfm-title.php
@@ -0,0 +1,7 @@
+<?php
+/*
+Plugin Name: Хлебные крошки
+Description: Хлебные кроки wp_title
+Version: 1.0
+Author: Toxa
+*/
