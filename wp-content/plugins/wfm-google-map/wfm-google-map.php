dbd319e63605bce8267446751678fbe7a643903a
Anton Kadantsev <kadantsev.anton@yandex.ru>
2016-02-18 16:23:24 +0300

    18

--- wp-content/plugins/wfm-google-map/wfm-google-map.php
+++ wp-content/plugins/wfm-google-map/wfm-google-map.php
@@ -6,14 +6,23 @@ Version: 1.0
 Author: Тоха
 Author URI: http://страница_автора_плагина
 */
-add_shortcode('test', 'wfm_test');
+add_shortcode('map', 'wfm_map');
 
-function wfm_test($atr, $cont){
-	/*$text = !empty($cont) ? $cont : 'Тестовые данные';
-	$user = isset($atr["user"]) ? $atr['user'] : 'Name';*/
-	$atr = shortcode_atts(array(
-		'user' => 'Name', 
-		'content' => !empty($cont) ? $cont : 'Тестовые данные'
-	), $atr);// функция wordpress, получить атрибуты шоркода [test user="Toxa"]Ваши данные:[/test]
-	return '<h3>'.$atr['content'].'</h3>Привет, '.$atr['user'];
+function wfm_map($atts, $content){
+	$atts = shortcode_atts(
+		array(
+			'center' => 'Ейск, Краснодарский край',
+			'width' => 600,
+			'height' => 300,
+			'zoom' => 13,
+			'content' => !empty($content) ? "<h2>$content</h2>" : "<h2>Карта от google</h2>"
+		), $atts
+	);
+	
+	$atts['size'] = $atts['width'].'x'.$atts['height'];
+	$atts['center'] = str_replace(' ', '+', $atts['center']);
+	extract($atts);
+	$map = $content;
+	$map .= "<img src='http://maps.googleapis.com/maps/api/staticmap?center=".$center."&zoom=".$zoom."&size=".$size."&sensor=false' alt=''>";
+	return $map;
 }
