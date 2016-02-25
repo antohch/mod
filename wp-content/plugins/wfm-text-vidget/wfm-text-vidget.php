9d36d7c8f6bd55e2a7a3d0d1bca99669d1024ff5
Anton Kadantsev <kadantsev.anton@yandex.ru>
2016-02-25 17:08:32 +0300

    2

--- wp-content/plugins/wfm-text-vidget/wfm-text-vidget.php
+++ wp-content/plugins/wfm-text-vidget/wfm-text-vidget.php
@@ -29,16 +29,39 @@ class WFM_Widget extends WP_Widget{
 		parent::__construct('wfm', '', $args);
 	}
 	
-	public function widget(){
+	public function widget($args, $instance){
+		extract($args);
+		extract($instance);
 		
+		$title = apply_filters('widget_title', $title);
+		$text = apply_filters('widget_text', $text);
+		
+		echo $before_widget;
+		echo $before_title . $title . $after_title;
+		echo $text;
+		echo $after_widget;
 	}
 	function form($instance){
 		$title = ! empty( $instance['title'] ) ? $instance['title'] : __( 'New title', 'text_domain' );
+		$text = ! empty( $instance['text'] ) ? $instance['text'] : __( 'Новый текст', 'text_domain' );
 		?>
 		<p>
 		<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label> 
 		<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>">
 		</p>
+
+		<p>
+		<label for="<?php echo $this->get_field_id( 'title' ); ?>">Текст</label> 
+		
+		<textarea cols="20" rows="5" class="widefat" id="<?php echo $this->get_field_id( 'text' ); ?>" name="<?php echo $this->get_field_name( 'text' ); ?>"><?php echo  $text; ?></textarea>
+		
 		<?php 
+	}
+	
+	function update($new_instance, $old_instance){
+		$new_instance['title'] = !empty($new_instance['title']) ? strip_tags($new_instance['title']) : '';
+		$new_instance['text'] = str_replace('<p>', '', $new_instance['text']);
+		$new_instance['text'] = str_replace('</p>', '', $new_instance['text']);
+		return $new_instance;
 	}
 }
