b2d7b642b4c8b7da8c404850588e4df15116447e
Anton Kadantsev <kadantsev.anton@yandex.ru>
2016-03-01 17:07:31 +0300

    3-2

--- wp-content/plugins/wfm-options/wfm_options.php
+++ wp-content/plugins/wfm-options/wfm_options.php
@@ -7,7 +7,7 @@ Version: Номер версии плагина, например: 1.0
 Author: Имя автора плагина
 Author URI: http://страница_автора_плагина
 */
-
+/*
 add_action("admin_init", 'wfm_first_option');
 
 function wfm_first_option(){
@@ -27,4 +27,41 @@ function wfm_option_cb(){
 ?>
 	<input type="text" name="wfm_first_option" id="wfm_first_option" value="<?php echo esc_attr(get_option('wfm_first_option'))?>" class='regular-text'>
 <?php
+}
+*/
+add_action( 'admin_init', 'wfm_theme_options' );
+
+function wfm_theme_options(){
+	register_setting( 'general', 'wfm_theme_options' );
+
+	// $id - ID секции
+	// $title - заголовок
+	// $callback - callback-функция для генерации HTML-кода
+	// $page - для какой страницы регистрируется секция
+	add_settings_section( 'wfm_theme_section_id', 'Опции темы', 'wfm_theme_options_section_cb', 'general' );
+
+	add_settings_field( 'wfm_theme_options_body', 'Цвет фона', 'wfm_theme_body_cb', 'general', 'wfm_theme_section_id' );
+	add_settings_field( 'wfm_theme_options_header', 'Цвет header', 'wfm_theme_header_cb', 'general', 'wfm_theme_section_id' );
+}
+
+function wfm_theme_options_section_cb(){
+	echo '<p>Описание секции</p>';
+}
+
+function wfm_theme_body_cb(){
+	$options = get_option('wfm_theme_options');
+	?>
+
+<input type="text" name="wfm_theme_options[wfm_theme_options_body]" id="wfm_theme_options_body" value="<?php echo esc_attr($options['wfm_theme_options_body']); ?>" class="regular-text">
+
+	<?php
+}
+
+function wfm_theme_header_cb(){
+	$options = get_option('wfm_theme_options');
+	?>
+
+<input type="text" name="wfm_theme_options[wfm_theme_options_header]" id="wfm_theme_options_header" value="<?php echo esc_attr($options['wfm_theme_options_header']); ?>" class="regular-text">
+
+	<?php
 }
