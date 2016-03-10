1595aa9020908763a9435c8c37d2f3a5d3c9b9f9
Anton Kadantsev <kadantsev.anton@yandex.ru>
2016-03-10 16:22:36 +0300

    option

---
+++ wp-content/plugins/wfm-options-page/wfm-options-page.php
@@ -0,0 +1,47 @@
+<?php
+/*
+Plugin Name: Options & Settings API
+Description: Изучаем API опций и настроек. Собственная страница настроек
+Plugin URI: http://webformyself.com
+Author: Андрей
+Author URI: http://webformyself.com
+*/
+
+add_action( 'admin_menu', 'wfm_admin_menu' );
+
+function wfm_admin_menu(){
+	// $page_title, $menu_title, $capability, $menu_slug, $function
+	add_options_page( 'Опции темы (title)', 'Опции темы', 'manage_options', 'wfm-theme-options', 'wfm_option_page' );
+}
+
+function wfm_option_page(){
+	if( !empty($_POST) && check_admin_referer('wfm-theme-options') ){
+		update_option( 'wfm_theme_options', array(
+				'wfm_theme_options_body' => $_POST['wfm_theme_options']['wfm_theme_options_body'],
+				'wfm_theme_options_header' => $_POST['wfm_theme_options']['wfm_theme_options_header']
+			)
+		);
+		echo '<div class="updated"><p><strong>Настройки сохранены.</strong></p></div>';
+	}
+	$options = get_option( 'wfm_theme_options' );
+	?>
+
+<div class="wrap">
+	<h2>Опции темы</h2>
+	<p>Настройки темы плагина Options &amp; Settings API</p>
+	<form action="" method="post">
+		<?php wp_nonce_field('wfm-theme-options'); ?>
+		<p>
+			<label for="wfm_theme_options_body">Цвет фона</label>
+			<input type="text" name="wfm_theme_options[wfm_theme_options_body]" id="wfm_theme_options_body" value="<?php echo esc_attr($options['wfm_theme_options_body']); ?>" class="regular-text">
+		</p>
+		<p>
+			<label for="wfm_theme_options_header">Цвет header</label>
+			<input type="text" name="wfm_theme_options[wfm_theme_options_header]" id="wfm_theme_options_header" value="<?php echo esc_attr($options['wfm_theme_options_header']); ?>" class="regular-text">
+		</p>
+		<?php submit_button(); ?>
+	</form>
+</div>
+
+	<?php
+}
